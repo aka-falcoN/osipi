b6f4191e8c57adff9f1f293c86668724f63db59a
Sumeet Chaurasia <csumeet@google.com>
2020-06-14 13:36:00 +0530

    needs changes in arch but proceeding with dev

--- deployment/collective_deployment/main.tf
+++ deployment/collective_deployment/main.tf
@@ -24,28 +24,6 @@ module "remote_state_bucket" {
 }
 
 # /******************************************
-# 	Google managed MS Active Directory
-#  *****************************************/
-
-resource "null_resource" "create_active_directory" {
-  triggers = {
-    project_id = var.project_id
-  }
-  provisioner "local-exec" {
-    command = "gcloud active-directory domains create ${var.ad_fqdn} --reserved-ip-range='172.16.0.0/24' --region=${var.region} --authorized-networks=${local.network_link} --project=${var.project_id}"
-  }
-}
-
-# resource "null_resource" "delete_active_directory" {
-#   triggers = {
-#     project_id = var.project_id
-#   }
-#   provisioner "local-exec" {
-#     command = "gcloud active-directory domains delete ${var.ad_fqdn} --reserved-ip-range='172.16.0.0/24' --region=${var.region} --authorized-networks=${local.network_name} --project=${var.project_id}"
-#   }
-# }
-
-# /******************************************
 # 	VPC configuration
 #  *****************************************/
 resource "google_compute_network" "vpc" {
@@ -75,6 +53,69 @@ resource "google_compute_subnetwork" "subnet" {
   private_ip_google_access = "true"
   region                   = var.region
 }
+
+
+# /******************************************
+# 	GCP managed MS Active Directory
+#  *****************************************/
+
+resource "null_resource" "create_active_directory" {
+  triggers = {
+    project_id = var.project_id
+  }
+  provisioner "local-exec" {
+    on_failure = "continue"
+    command << EOF    
+      gcloud active-directory domains create ${var.ad_fqdn} --reserved-ip-range='172.16.0.0/24' --region=${var.region} --authorized-networks=${local.network_link} --project=${var.project_id} &
+      sleep 65m \
+      && gcloud active-directory domains reset-managed-identities-admin-password ${var.ad_fqdn} --quiet --project=${var.project_id} > ad_secret
+    EOF
+  }
+}
+
+data "local_file" "ad_secret" {
+    filename = "./ad_secret"
+
+    depends_on=[null_resource.create_active_directory]
+}
+
+module "active_directory_secret" {
+  source = "../../modules/terraform/secret_manager"
+
+  project_id            = var.project_id
+  secret_id             = "active_directory_secret"
+  automatic_replication = "true"
+  secretAccessor        = [module.osipi_server.service_account_iam_email, module.osipi_integrator.service_account_iam_email]
+  secret_data = data.local_file.ad_secret.content
+}
+
+resource "null_resource" "delete_active_directory" {
+  when    = "destroy"
+
+  provisioner "local-exec" {
+    command = "gcloud active-directory domains delete ${var.ad_fqdn}  --project=${var.project_id}"
+  }
+}
+
+resource "google_dns_managed_zone" "private_dns_zone" {
+  name        = "ad-private-dns-zone"
+  dns_name    = "${var.ad_fqdn}."
+  description = "Private Zonal Cloud DNS for MS Active Directory"
+
+  visibility = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = google_compute_network.vpc.self_link
+    }
+  }
+
+  depends_on=[null_resource.create_active_directory]
+}
+
+# /******************************************
+# 	OSI PI Components
+#  *****************************************/
 
 module "osipi_server" {
   source = "../../modules/osipi/server"
